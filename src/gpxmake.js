3ac8928823677f0aa48d3a9d5e29ea38b3fc7a25
feyyazakks <feyyazakkus@gmail.com>
2016-11-27 22:46:09 +0300

    add: gpx metadata elements

--- src/gpxmake.js
+++ src/gpxmake.js
@@ -6,190 +6,311 @@
  * @author: Feyyaz Akkuş
  *
  */
-(function (root, factory) {
+(function(root, factory) {
 
-    if (typeof exports !== 'undefined') {
-        if (typeof module !== 'undefined' && typeof module.exports) {
-            exports = module.exports = factory(root, exports);
-        }
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && typeof module.exports) {
+      exports = module.exports = factory(root, exports);
     }
-    else if (typeof define === 'function' && define.amd) {
-        define(['exports'], function (exports) {
-            root.GpxMake = factory(root, exports);
-        });
+  } else if (typeof define === 'function' && define.amd) {
+    define(['exports'], function(exports) {
+      root.GpxMake = factory(root, exports);
+    });
+  } else {
+    root.GpxMake = factory();
+  }
+
+}(this, function() {
+
+  'use strict';
+
+  var elements = {
+    'rteType': ['name', 'cmt', 'desc', 'src', 'link', 'number', 'type'],
+    'trkType': ['name', 'cmt', 'desc', 'src', 'link', 'number', 'type'],
+    'wptType': ['lat', 'lon', 'ele', 'time', 'magvar', 'geoidheight', 'name', 'cmt', 'desc', 'src', 'url', 'urlname', 'sym', 'type', 'fix', 'sat', 'hdop', 'vdop', 'pdop', 'ageofdgpsdata', 'dgpsid']
+  }
+
+  var GpxMake = (function() {
+
+    var defaults = {
+      version: '1.1',
+      creator: 'gpxmake'
+    };
+
+    var fileOptions = {};
+
+    // consructor
+    function GpxMake(options) {
+
+    	this.metadata = '';
+      this.track = '';
+      this.route = '';
+      this.wayPoints = '';
+
+      //fileOptions = utils.extend(defaults, options);
+      fileOptions = options;
+
+      if (fileOptions.metadata) {
+      	this.metadata = setMetaData(fileOptions.metadata);
+      }
+
+      if (fileOptions.track) {
+        this.track = setTrack(fileOptions.track);
+      }
     }
-    else {
-        root.GpxMake = factory(root);
+
+    // download file
+    GpxMake.prototype.download = function(filename) {
+
+      filename = filename + '.gpx';
+
+      var content = setFileContent(this.metadata, this.track);
+
+      var blob = new Blob([content], {
+        'type': 'application/xml'
+      });
+
+      var link = document.createElement('a');
+      document.body.appendChild(link);
+
+      // detect browser 
+      if (!utils.detectIE()) { // other browsers	    	
+        link.href = window.URL.createObjectURL(blob);
+        link.download = filename;
+        link.click();
+
+      } else { // internet explorer
+        window.navigator.msSaveOrOpenBlob(blob, filename);
+      }
     }
 
-}(this, function () {
+    function setMetaData(metadata) {
 
- 	'use strict';
+    	var meta = '<metadata>\n';
 
-    var elements = {
-      'gpx': ['name', 'desc', 'author', 'url', 'urlname', 'time', 'keywords', 'bounds'],
-      'rte': ['name', 'cmt', 'desc', 'src', 'url', 'urlname', 'number'],
-      'trk': ['name', 'cmt', 'desc', 'src', 'url', 'urlname', 'number'],
-      'points': ['lat', 'lon', 'ele', 'time', 'magvar', 'geoidheight', 'name', 'cmt', 'desc', 'src', 'url', 'urlname', 'sym', 'type', 'fix', 'sat', 'hdop', 'vdop', 'pdop', 'ageofdgpsdata', 'dgpsid']
-    }
+    	// name
+    	if (metadata.name) {
+    		meta += '<name>' + metadata.name + '</name>\n';
+    	}
+    	// desc
+    	if (metadata.desc) {
+    		meta += '<desc>' + metadata.desc + '</desc>\n';
+    	}
+    	// time
+    	if (metadata.time) {
+    		meta += '<time>' + metadata.time + '</time>\n';
+    	}
+    	// keywords
+    	if (metadata.keywords) {
+    		meta += '<keywords>' + metadata.keywords + '</keywords>\n';
+    	}
+    	// author
+    	var author = metadata.author;
+    	if (author) {
+    		meta += '<author>\n';
+    		if (author.name) {
+    			meta += '<name>' + author.name + '</name>\n';
+    		}
+    		if (author.email) {
+    			if (author.email.id && author.email.domain) {
+    				meta += '<email id="'+author.email.id+'" domain="'+author.email.domain+'" />\n';	
+    			} else {
+    				console.warn("<email> element should have id and domain information.");
+    			}
+    		}
+    		if (author.link) {
+    			if (author.link.href) {
+    				meta += '<link href="'+author.link.href+'">\n';
 
-	var GpxMake = (function () {
-		
-		var defaults = {
-			version: '1.1',
-			creator: 'gpxmake'
-		};
+    				if (author.link.text) {
+    					meta += '<text>' + author.link.text + '</text>\n';
+    				}
+    				if (author.link.type) {
+    					meta += '<type>' + author.link.type + '</type>\n';
+    				}
+    				meta += '</link>\n';
+    			} else {
+    				console.warn("<link> element should have href information.");
+    			}
+    		}
+    		meta += '</author>\n';
+    	}
 
-        var fileOptions = {};
+    	// copyright
+    	var copyright = metadata.copyright;
+    	if (copyright) {
+    		if (copyright.author) {
+    			meta += '<copyright author="' + copyright.author + '">\n';
+    			
+    			if (copyright.year) {
+  					meta += '<year>' + copyright.year + '</year>\n';
+  				}
+  				if (copyright.licence) {
+  					meta += '<licence>' + copyright.licence + '</licence>\n';
+  				}
+  				meta += '</copyright>\n';
+    		} else {
+    			console.warn("<copyright> element should have author information.");
+    		}
+    	}
 
-		// consructor
-		function GpxMake(options) {
+    	// link
+    	var link = metadata.link;
+    	if (link) {
+				if (link.href) {
+					meta += '<link href="' + link.href + '">\n';
 
-			this.track = '';
-			this.route = '';
-
-			fileOptions = utils.extend(defaults, options);
-
-			if (fileOptions.track) {
-				this.track = setTrack(fileOptions.track);
-			}
-		}
-		
-     	// download file
-		GpxMake.prototype.download = function (filename) {
-
-            filename = filename + '.gpx';
-
-            var content = setFileContent(fileOptions, this.track);
-
-			var blob = new Blob([content], {'type': 'application/xml'});
-
-			var link = document.createElement('a');
-			document.body.appendChild(link); // for firefox it's necessary
-
-		    // detect browser 
-		    if (!utils.detectIE()) { // other browsers	    	
-		    	link.href = window.URL.createObjectURL(blob);
-		    	link.download = filename;
-		    	link.click();
-
-		    } else { // internet explorer
-		    	window.navigator.msSaveOrOpenBlob(blob, filename);
-		    }
-		}
-
-		// set track <trk></trk>
-		function setTrack(track) {
-
-            var trk = '<trk>\n';
-            // add optinal elements
-		    for (var key in track) {
-		      if (typeof(track[key]) == 'string') {
-		        if (elements.trk.indexOf(key) != -1) {
-		          trk += '<' + key + '>' + track[key] + '</' + key + '>\n';
-		        } else {
-		          console.warn("'" + key + "' is not a valid gpx file element.");
-		        }
-		      }
-		    }
-
-            trk += '<trkseg>\n';
-
-            var trkpts = track.points;
-
-            if (trkpts) {
-                for (var i = 0; i < trkpts.length; i++) {
-
-				  var lat = trkpts[i]['lat'];
-				  var lon = trkpts[i]['lon'];
-
-				  if (lat && lon) {
-				    // add lat, long required elements
-				    trk += '<trkpt lat="' + lat + '" lon="' + lon + '">\n';
-
-				    // add optinal elements
-				    for (var key in trkpts[i]) {
-				      if (key != 'lat' && key != 'lon') {
-				        if (elements.points.indexOf(key) != -1) {
-				          trk += '<' + key + '>' + trkpts[i][key] + '</' + key + '>\n';
-				        } else {
-				          console.warn("'" + key + "' is not a valid gpx file element.");
-				        }
-				      }
-				    }
-
-				    // close trkpt element
-				    trk += '</trkpt>\n'
-
-				  } else {
-				    throw "Trackpoints should have lat and lon information. Please reorganize your data.";
-				  }
-
+					if (link.text) {
+						meta += '<text>' + link.text + '</text>\n';
+					}
+					if (link.type) {
+						meta += '<type>' + link.type + '</type>\n';
+					}
+					meta += '</link>';
+				} else {
+					console.warn("<link> element should have href information.");
 				}
-            } else {
-                console.warn("No track points defined");
+			}
+
+			// bounds
+			var bounds = metadata.bounds;
+			if (bounds) {
+				if (bounds.minlat && bounds.minlon && bounds.maxlat && bounds.maxlon) {
+					meta += '<bounds minlat="' + bounds.minlat + '" minlon="' + bounds.minlon + '" maxlat="' + bounds.maxlat + '" maxlon="' + bounds.maxlon + '" />\n';
+				} else {
+					console.warn("<bounds> element should have minlat, minlon, maxlat and maxlon information.");
+				}
+			}
+
+			meta += '</metadata>\n';
+    	return meta;
+    }
+
+    // set track <trk></trk>
+    function setTrack(track) {
+
+      var trk = '<trk>\n';
+      // add optinal elements
+      for (var key in track) {
+        if (typeof(track[key]) == 'string') {
+          if (elements.trkType.indexOf(key) != -1) {
+            trk += '<' + key + '>' + track[key] + '</' + key + '>\n';
+          } else {
+            console.warn("'" + key + "' is not a valid gpx file element.");
+          }
+        }
+      }
+
+      trk += '<trkseg>\n';
+
+      var trkpts = track.points;
+
+      if (trkpts) {
+        for (var i = 0; i < trkpts.length; i++) {
+
+          var lat = trkpts[i]['lat'];
+          var lon = trkpts[i]['lon'];
+
+          if (lat && lon) {
+            // add lat, long required elements
+            trk += '<trkpt lat="' + lat + '" lon="' + lon + '">\n';
+
+            // add optinal elements
+            for (var key in trkpts[i]) {
+              if (key != 'lat' && key != 'lon') {
+                if (elements.wptType.indexOf(key) != -1) {
+                	if (key == 'link') {
+                		var link = trkpts[i]['link'];
+                		if (link.href) {
+                			trk += '<link href="' + link.href + '">';
+                			if (link.text) {
+												trk += '<text>' + link.text + '</text>\n';
+											}
+											if (link.type) {
+												trk += '<type>' + link.type + '</type>\n';
+											}
+                			trk += '</link>'
+                		}
+                	} else {
+                		trk += '<' + key + '>' + trkpts[i][key] + '</' + key + '>\n';	
+                	}
+                  
+                } else {
+                  console.warn("'" + key + "' is not a valid gpx file element.");
+                }
+              }
             }
 
-            trk += '</trkseg>\n';
-            trk += '</trk>\n';
+            // close trkpt element
+            trk += '</trkpt>\n'
 
-            return trk;
+          } else {
+            throw('Trackpoints should have lat and lon information. Please reorganize your data.');
+          }
         }
 
-        // set xml file content
-		function setFileContent(options, track) {
+      } else {
+        console.warn("No track points defined");
+      }
 
-			var xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
-    			'<gpx xsi:schemaLocation="http://www.topografix.com/GPX/1/1 ' + 
-    			'http://www.topografix.com/GPX/1/1/gpx.xsd" ' + 
-    			'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' + 
-    			'version="' + fileOptions.version + '" ' +
-                'creator="' + fileOptions.creator + '" ' + 
-    			'xmlns="http://www.topografix.com/GPX/1/1">\n';
+      trk += '</trkseg>\n';
+      trk += '</trk>\n';
 
-    		// add optional file informations
-               
-            xml += track;
-			xml += '</gpx>';
+      return trk;
+    }
 
-			return xml;
-		}
+    // set xml file content
+    function setFileContent(metadata, track) {
+      
+      var xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
+        '<gpx xsi:schemaLocation="http://www.topografix.com/GPX/1/1 ' +
+        'http://www.topografix.com/GPX/1/1/gpx.xsd" ' +
+        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+        'version="1.1" creator="gpxmake.js" ' + 
+        'xmlns="http://www.topografix.com/GPX/1/1">\n';
 
-		// helpers
-		var utils = (function() {
+      xml += metadata;
+      xml += track;
+      xml += '</gpx>';
 
-			return {
+      return xml;
+    }
 
-				extend: function() {
-					for (var i = 1; i < arguments.length; i++) {
-						for (var key in arguments[i]) {
-							if (arguments[i].hasOwnProperty(key)) {
-								arguments[0][key] = arguments[i][key];
-							}
-						}
-					}
-					return arguments[0];
-				},
+    // helpers
+    var utils = (function() {
 
-				detectIE: function () {
-				    
-				    var ua = window.navigator.userAgent;
-				    var msie = ua.indexOf('MSIE ');
-				    var trident = ua.indexOf('Trident/');
-				    var edge = ua.indexOf('Edge/');
+      return {
 
-				    // internet explorer
-				    if (msie > 0 || trident > 0 || edge > 0) {
-				    	return true;
-				    }
-				    // other browser
-				    return false;
-				}
-			}
-		}());
+        extend: function() {
+          for (var i = 1; i < arguments.length; i++) {
+            for (var key in arguments[i]) {
+              if (arguments[i].hasOwnProperty(key)) {
+                arguments[0][key] = arguments[i][key];
+              }
+            }
+          }
+          return arguments[0];
+        },
 
-		return GpxMake;
-	})();
+        detectIE: function() {
+
+          var ua = window.navigator.userAgent;
+          var msie = ua.indexOf('MSIE ');
+          var trident = ua.indexOf('Trident/');
+          var edge = ua.indexOf('Edge/');
+
+          // internet explorer
+          if (msie > 0 || trident > 0 || edge > 0) {
+            return true;
+          }
+          // other browser
+          return false;
+        }
+      }
+    }());
 
     return GpxMake;
+  })();
+
+  return GpxMake;
 }));
